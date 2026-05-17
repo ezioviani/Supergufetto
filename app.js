e031aef4e28ba0929fd5d769f588a28e9192d168
ezioviani <ezioviani@gmail.com>
2026-05-17 22:45:12 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
+console.log("Supergufetto: JS caricato!");
+
 const captureBtn = document.getElementById("captureBtn");
 const cameraInput = document.getElementById("cameraInput");
 const textContainer = document.getElementById("textContainer");
 
 captureBtn.addEventListener("click", () => {
-  console.log("Click su Scatta una foto");
+  textContainer.textContent = "Click ricevuto!";
   cameraInput.click();
 });
 
 cameraInput.addEventListener("change", (event) => {
   const file = event.target.files[0];
   if (!file) {
-    textContainer.textContent = "Nessun file selezionato.";
+    textContainer.textContent = "Nessuna foto.";
     return;
   }
   textContainer.textContent = "Foto selezionata: " + file.name;
